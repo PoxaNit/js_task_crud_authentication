709a4e8469fc15dc049fa9063eb73a139e73d2ca
Júlio <jc.5047792@gmail.com>
2025-12-05 18:28:00 -0400

    I started to make the router

---
+++ routes/Router.js
@@ -0,0 +1,16 @@
+import response from "../helpers/response.js";
+
+ class Router
+{
+
+    static async handle (uri, request body) {
+
+        switch (uri) {
+
+            case "/users"
+
+        }
+
+    }
+
+}
