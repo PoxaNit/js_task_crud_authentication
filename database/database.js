863d483ddde3959da648a6b621f7c142e1c0ffd4
Júlio <jc.5047792@gmail.com>
2025-12-05 23:39:21 -0400

    screw it

--- database/database.js
+++ database/database.js
@@ -7,10 +7,6 @@ import { fileURLToPath } from "node:url";
  const __filename = fileURLToPath(import.meta.url);
  const __dirname = path.dirname(__filename);
 
- dotenv.config({
-   path: path.resolve(__dirname, "../.env")
- });
-console.log(`port: ${process.env.dbport}`)
  const pool = await mariadb.createPool({
    port: process.env.dbport,
    host: process.env.dbhost,
